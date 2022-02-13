664ffefa9400a0563229878b2cf272ef93b43e00
ChunhaoZhu <15962613747@163.com>
2022-02-13 12:49:52 -0800

    seperate req handling

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
 app.get('/web', (req, res) => {
     res.send("hello web");
 });
