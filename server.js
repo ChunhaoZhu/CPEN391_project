e85462ef9187d75cef8176d4faa014a6a37a7ea6
ChunhaoZhu <15962613747@163.com>
2022-02-13 12:33:52 -0800

    try ports

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
+const app1 = express();
+const app2 = express();
 
-app.get('/', (req, res) => {
+app1.get('/web', (req, res) => {
     res.send("hello");
 });
 
-app.listen(process.env.PORT, () => {
+app1.listen(process.env.PORT, () => {
     console.log("mmm");
+})
+
+app2.get('/de1', (req, res) => {
+    res.send("hello de1");
+});
+
+app2.listen(process.env.PORT, () => {
+    console.log("nnn");
 })
